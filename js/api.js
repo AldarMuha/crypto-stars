73e299cf7564b82402de0bb5713f2a3c6488b209
Aldar Muha <aldarmuha1992@gmail.com>
2024-09-13 00:24:39 +0500

    Добавил файл api.js, в котором добавил методы получения данных с сервера и отправки данных на сервер

---
+++ js/api.js
@@ -0,0 +1,49 @@
+const getUser = async (onSuccess, onError) => {
+  try {
+    const response = await fetch(
+      'https://cryptostar.grading.htmlacademy.pro/user'
+    );
+    if (!response.ok) {
+      onError();
+    }
+    const data = await response.json();
+    onSuccess(data);
+  } catch (error) {
+    onError();
+  }
+};
+
+const getContractors = async (onSuccess, onError) => {
+  try {
+    const response = await fetch(
+      'https://cryptostar.grading.htmlacademy.pro/contractors'
+    );
+    if (!response.ok) {
+      onError();
+    }
+    const data = await response.json();
+    onSuccess(data);
+  } catch (error) {
+    onError();
+  }
+};
+
+const sendData = async (onSuccess, onFail, body) => {
+  try {
+    const response = await fetch(
+      'https://cryptostar.grading.htmlacademy.pro/',
+      {
+        method: 'POST',
+        body,
+      },
+    );
+    if (!response.ok) {
+      onFail();
+    }
+    onSuccess();
+  } catch (error) {
+    onFail();
+  }
+};
+
+export { getUser, getContractors, sendData };
